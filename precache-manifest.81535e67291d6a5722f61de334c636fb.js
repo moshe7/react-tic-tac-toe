ca23b62c99f618cd911147bf84de26a35efe5390
moshe <7moshe7@gmail.com>
2020-04-13 00:46:46 +0300

    Updates

---
+++ precache-manifest.81535e67291d6a5722f61de334c636fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def9af573a6c81c4b8ce2aa97e6fd645",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "32ead17cad48ca33bc92",
+    "url": "/react-tic-tac-toe/static/css/main.5ac90d4c.chunk.css"
+  },
+  {
+    "revision": "210717bcd6136923a012",
+    "url": "/react-tic-tac-toe/static/js/2.5ccea08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.5ccea08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ead17cad48ca33bc92",
+    "url": "/react-tic-tac-toe/static/js/main.b0d3bb38.chunk.js"
+  },
+  {
+    "revision": "0b0e67b6f19b506ccc93",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.0d324d13.js"
+  }
+]);
